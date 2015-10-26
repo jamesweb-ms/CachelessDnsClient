2fd81d7a644e37336c8a8d39d9b3bf86e64dc174
jamesweb-ms <jamesweb@microsoft.com>
2015-10-26 16:14:36 -0700

    Change to use Heijden.Dns (no crypto dependency)

--- CachelessDnsClient/Client.cs
+++ CachelessDnsClient/Client.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
-using ARSoft.Tools.Net;
-using ARSoft.Tools.Net.Dns;
+using Heijden.DNS;
 
 namespace CachelessDnsClient
 {
     public class Client
     {
+        Resolver localResolver;
+
+        public Client()
+        {
+            this.localResolver = new Resolver(Resolver.GetDnsServers());
+        }
+
         /// <summary>
         /// Does a simple lookup against the SOA to see if record exists.
         /// Good for initial check for name exisitance
@@ -26,49 +34,73 @@ namespace CachelessDnsClient
             return GetHostEntry(name, true);
         }
 
+        static IPAddress GetLocalDnsServerAddress()
+        {
+            var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var networkInterface in networkInterfaces)
+            {
+                if (networkInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    var networkProperties = networkInterface.GetIPProperties();
+                    if (networkProperties.DnsAddresses.Count > 0)
+                    {
+                        return networkProperties.DnsAddresses.First();
+                    }
+                }
+            }
+            throw new ArgumentException("No DNS servers found");
+        }
+
         async Task<bool> GetHostEntry(string name, bool replicas)
         {
-            DomainName hostName = null;
-            if (!DomainName.TryParse(name, out hostName))
-            {
-                throw new ArgumentException(string.Format("'{0}' is not a valid domain name", name));
-            }
+            HostName hostName = new HostName(name);
 
             // Get initial Master server to do direct queries
-            DomainName hostDomain = hostName.GetParentName();
-            IDnsResolver resolver = new DnsStubResolver();
-            var soaRecords = await resolver.ResolveAsync<SoaRecord>(hostDomain, RecordType.Soa);
-            if (soaRecords != null && soaRecords.Count > 0)
+            HostName hostDomain = hostName.GetParent();
+            if (hostDomain == null)
+            {
+                throw new ArgumentException(string.Format("'{0}' is not a valid host name", name));
+            }
+            var soaRecords = localResolver.Query(hostDomain.Name, QType.SOA);
+            if (soaRecords != null && soaRecords.RecordsSOA.Length > 0)
             {
                 // Some local servers provide truncated SOA list, so query authority
-                var hostDomainIps = await resolver.ResolveHostAsync(soaRecords[0].MasterName);
-                var ip4s = hostDomainIps.Where(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
-                DnsClient client = new DnsClient(ip4s, 60);
-                var soaMessage = client.Resolve(hostDomain, RecordType.Soa);
-
-                if (soaMessage != null && soaMessage.ReturnCode == ReturnCode.NoError && soaMessage.AuthorityRecords.Count > 0)
+                var hostDomainIps = await localResolver.ResolveAsync(soaRecords.RecordsSOA[0].MNAME);
+                var remoteResolver = new Resolver(GetIpV4Address(hostDomainIps))
                 {
+                    Recursion = true,
+                    TimeOut = 20,
+                    TransportType =  Heijden.DNS.TransportType.Tcp
+                };
+
+                var soaMessage = remoteResolver.Query(hostDomain.Name, QType.SOA);
+
+                if (soaMessage != null && soaRecords.RecordsSOA.Length > 0)
+                {
+                    // Search each SOA record
                     bool foundInReplicas = true;
-                    foreach (NsRecord server in soaMessage.AuthorityRecords)
+                    foreach (var server in soaMessage.RecordsSOA)
                     {
-                        // Create client talking directly to Master server
-                        hostDomainIps = await resolver.ResolveHostAsync(server.NameServer);
-                        ip4s = hostDomainIps.Where(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
-                        client = new DnsClient(ip4s, 60);
-
-                        // NOTE: Async client.Resolve appears not to work, so using sync method
-                        var dnsMessage = client.Resolve(hostName, RecordType.A, RecordClass.INet, new DnsQueryOptions() { IsRecursionDesired = true });
-                        if (dnsMessage == null || dnsMessage.ReturnCode == ReturnCode.NxDomain || dnsMessage.AnswerRecords.Count == 0)
+                        // Search each IP associated with NS Server
+                        hostDomainIps = localResolver.GetHostByName(server.MNAME);
+                        foreach (var ipEndpoint in GetIpV4Address(hostDomainIps))
                         {
-                            // Record not found
-                            Console.WriteLine("Record not found in NS server '{0}'", server.NameServer);
-                            foundInReplicas = false;
-                        }
+                            remoteResolver = new Resolver(ipEndpoint);
+                            remoteResolver.Recursion = true;
+                            remoteResolver.UseCache = false;
+                            var dnsMessage = remoteResolver.Query(hostName.Name, QType.A);
+                            if (dnsMessage == null || !string.IsNullOrEmpty(dnsMessage.Error) || dnsMessage.Answers.Count == 0)
+                            {
+                                // Record not found
+                                Console.WriteLine("Record not found in NS server '{0}'", server.MNAME);
+                                foundInReplicas = false;
+                            }
 
-                        // If we aren't checking all replica's, return true from first result
-                        if (!replicas)
-                        {
-                            return foundInReplicas;
+                            // If we aren't checking all replica's, return true from first result
+                            if (!replicas)
+                            {
+                                return foundInReplicas;
+                            }
                         }
                     }
 
@@ -79,6 +111,17 @@ namespace CachelessDnsClient
 
             // No SOA record found
             return false;
+        }
+
+        IPEndPoint[] GetIpV4Address(IPHostEntry hostEntry)
+        {
+            var ip4Addresses = hostEntry.AddressList.Where(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+            List<IPEndPoint> endpoints = new List<IPEndPoint>();
+            foreach (var address in ip4Addresses)
+            {
+                endpoints.Add(new IPEndPoint(address, Resolver.DefaultPort));
+            }
+            return endpoints.ToArray();
         }
     }
 }
---
+++ CachelessDnsClient/DnsExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Heijden.DNS;
+
+namespace CachelessDnsClient
+{
+    public static class DnsExtension
+    {
+        public static Task<IPHostEntry> ResolveAsync(this Resolver resolver, string name)
+        {
+            if (resolver == null)
+            {
+                throw new ArgumentNullException("resolver");
+            }
+
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+            return Task<IPHostEntry>.Factory.FromAsync(resolver.BeginResolve, resolver.EndResolve, name, null);
+        }
+    }
+}
